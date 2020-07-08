816185483e34eeb17369c60586b690c9b31b5218
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-07-08 09:51:30 +0200

    substitut module created and completed

---
+++ substitut.py
@@ -0,0 +1,6 @@
+#-*-coding:utf-8 -*
+
+class Substitut:
+    def __init__(self, product):
+        self.product_product_id = products.id_product
+        self.substitut_product_id = products.id_product
