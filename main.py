3377ca3fc64f58b4f75478767daff2fac251ac0f
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-07-13 15:15:32 +0200

    adding intialisation module

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ import database
 
 def main():
     database_instance= database.Database()
-    # database.Database.create_database(database_instance)
-    #database.Database.check_database(database_instance)
+
+    database.Database.check_database(database_instance)
     database.Database.check_tables(database_instance)
-    #menu.Menu.show(database_instance)
+
+    if database_instance.database_status or\
+    database_instance.table_status:
+        menu.Menu.show(database_instance)
+    else:
+        reset.Reset.reset(database_instance)
+
 
 
 
