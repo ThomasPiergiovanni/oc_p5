178349e96400afd6f7e0d162746d6bec2343e557
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-07-13 19:18:05 +0200

    nedd update on quit module

--- menu.py
+++ menu.py
@@ -6,6 +6,7 @@ import registration
 import reset
 import quit
 
+
 class Menu:
     def show (database_instance):
         question = input("What do you want to do (choose one of the bellow number)?\
@@ -13,17 +14,21 @@ class Menu:
         \n 2 - See your saved substitutes\
         \n 3 - Reinitiate the database \
         \n 4 - Quit the program \n")
-        question = int(question)
-        if question == 1:
-            research.Research.research(database_instance)
-            Menu.show(database_i1nstance)
-        elif question == 2:
-            registration.Registration.regi(database_instance)
-            Menu.show(database_instance)
-        # elif question == 3:
-        #     reset.Reset.reset(database_instance)
-        elif question == 4:
-            quit.Quit.show()
-        else :
-            print ("Only number from 1 to 4 can be used. Retry ")
+        try:
+            question = int(question)
+            if question == 1:
+                research.Research.research(database_instance)
+                Menu.show(database_i1nstance)
+            elif question == 2:
+                registration.Registration.regi(database_instance)
+                Menu.show(database_instance)
+            # elif question == 3:
+            #     reset.Reset.reset(database_instance)
+            elif question == 4:
+                quit.Quit.show()
+            else :
+                print ("Only number from 1 to 4 can be used. Retry ")
+                Menu.show(database_instance)
+        except:
+            print ("Only NUMBERS can be used. Retry ")
             Menu.show(database_instance)
