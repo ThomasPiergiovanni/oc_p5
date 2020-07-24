d763332d90a6da5d94a6078ef7bc7c25cd86df7e
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-07-24 23:11:15 +0200

    pylint on reset.py

--- reset.py
+++ reset.py
@@ -1,11 +1,14 @@
-#-*-coding:utf-8 
-
+#-*-coding:utf-8
+"""Reset module.
+"""
 from categories import Categories
 from products import Products
 from substitutes import Substitutes
 import menu
 
 class Reset:
+    """Reset class.
+    """
     def __init__(self, database):
         self.database = database
         self.database.reset_nominal_scenario()
@@ -13,8 +16,7 @@ class Reset:
         self.categories.reset_nominal_scenario()
         self.products = Products(self.categories)
         self.products.reset_nominal_scenario()
-        self.substitutes = Substitutes (self.products)
+        self.substitutes = Substitutes(self.products)
         self.substitutes.reset_nominal_scenario()
         self.menu = menu.Menu(self.database)
         self.menu.menu_nominal_scenario()
-        
