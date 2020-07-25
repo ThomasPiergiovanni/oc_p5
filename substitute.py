10506b44645520f8726e505b9b85232b731a8005
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-07-25 21:30:31 +0200

    pylint on substitutes

--- substitute.py
+++ substitute.py
@@ -1,6 +1,9 @@
 #-*-coding:utf-8 -*
-
+"""Substitute module.
+"""
 class Substitute:
+    """Substitute class.
+    """
     def __init__(self, product_product_id, substitute_product_id):
         self.product_product_id = product_product_id
         self.substitute_product_id = substitute_product_id
