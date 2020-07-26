25ae4de37cc25c27ff87110b1df993a0881c4ddc
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-07-26 17:20:37 +0200

    updating config file

--- programm/admin/config.py
+++ programm/admin/config.py
@@ -1,41 +1,55 @@
 #-*-coding:utf-8 -*
 """Programm congiguration file"""
 
-# Description:OpenFoodActs categories endpoint.
+# Description: OpenFoodActs categories endpoint. It returns
+# the categories list.
 # Mandatory : Yes.
-# Settings: "https://fr-en.openfoodfacts.org/categories.json" (default).
-# Comments: Endpoint return the products categories list.
+# Default settings: "https://fr-en.openfoodfacts.org/categories.json".
+# Comments: Default settings cannot be modified. For more information
+# on the API endpoint please check "https://documenter.getpostman.com/view/
+# 8470508/SVtN3Wzy?version=latest#intro".
 CATEGORIES_ENDPOINT = "https://fr.openfoodfacts.org/categories.json"
 
-# Description:OpenFoodActs products per category ENDPOINT.
+# Description: OpenFoodActs products endpoint.It return the
+# products per category list.
 # Mandatory : Yes.
-# Settings: "https://fr-en.openfoodfacts.org/categories.json" (default).
-# Comments: Endpoint return the products categories list.
+# Default settings: "https://fr.openfoodfacts.org/cgi/search.pl".
+# Comments: Default settings cannot be modified. For more information
+# on the API endpoint please check "https://documenter.getpostman.com/view/
+# 8470508/SVtN3Wzy?version=latest#intro".
 PRODUCTS_ENDPOINT= "https://fr.openfoodfacts.org/cgi/search.pl"
 
 
-# Description:Headers.
+# Description: Headers, i.e. the application general informations, among other,
+# its name and version number.
 # Mandatory : Yes.
-# Settings: {'User-Agent': 'ThomasApp - Web - Version 0.0'} (default).
-# Comments: Application general inforamtions.
-HEADER = {'User-Agent': 'ThomasApp - Web - Version 0.0'}
+# Default settings: {'User-Agent': 'HealthyProductApp - Web - Version 0.0'}.
+# Comments: Settings can be changed but must kee the following structure
+# {'User-Agent': 'your informations'}
+HEADER = {'User-Agent': 'HealthyProductApp - Web - Version 0.0'}
 
-# Description:Database name.
+# Description: The database name you want to use for the application.
 # Mandatory : Yes.
-# Settings: project_5 (default).
-# Comments: Can be changed. If a database with that name already exist, it will be overwritten.
+# Default settings: "HPA" (HealthyProduuctApp).
+# Comments: Can be changed. Note that if a database using that name already
+# exists, it will be overwritten.
 DATABASE_NAME= "project_6"
 
-# Description:Host server.
+# Description: Host name i.e. The server on which MySQL is running.
 # Mandatory : Yes.
-# Settings: localhost (default).
-# Comments: Can be changed if server is not on local machine.
+# Default settings: "localhost".
+# Comments: Can be an IP adress as well if the MySQL server is not the 
+# local machine. Please check "https://dev.mysql.com/doc/connector-python/en/"
+# for more informations.
 HOST= "localhost"
 
-# Description: Username for DB connection.
+# Description: The user name used to authenticate with the MySQL server.
 # Mandatory : Yes.
-# Settings: root (default).
-# Comments: username.
+# Default settings: "root".
+# Comments: Can be changed but user must have at least the following MySQL privileges:
+# CREATE, DROP, EXECUTE, INSERT, REFERENCES, SELECT, SHOW DATABASE. 
+# Please check "https://dev.mysql.com/doc/refman/5.7/en/grant.html#grant-privileges"
+# for more informations on privilveges.
 USER= "root"
 
 # Description: Password for DB connection.
