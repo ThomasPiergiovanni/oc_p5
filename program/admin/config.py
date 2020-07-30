5ad9a7c42312e5a52568f19c50a84d8d23ed5dbd
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-07-30 22:43:59 +0200

    spelling corrected on config.py

--- program/admin/config.py
+++ program/admin/config.py
@@ -3,21 +3,21 @@
 """
 
 # DESCRIPTION: OpenFoodFacts (OFF) API categories list endpoint. It returns
-# the catgories list per country.
+# the categories list per country.
 # MANDATORY: Yes.
 # DEFAULT SETTINGS: "https://fr.openfoodfacts.org/categories.json".
-# CUSTOM SETTINGS: To change the country, use the appropriate  ISO-3166-1
-# Alpha 2 code and replace it in the endpoint(e.g.
-# "https://es.openfoodfacts.org/categories.json" for spain).
+# CUSTOM SETTINGS: To change the country, use the appropriate ISO-3166-1
+# Alpha 2 code and replace it in the endpoint (e.g.
+# "https://es.openfoodfacts.org/categories.json" for Spain).
 # For more information, please check "https://documenter.getpostman.com/view/
 # 8470508/SVtN3Wzy?version=latest#intro".
 CATEGORIES_ENDPOINT = "https://fr.openfoodfacts.org/categories.json"
 
 # DESCRIPTION: OFF API products research functionality endpoint.
-# It return the product research functionality per country.
+# It returns the product research functionality per country.
 # MANDATORY: Yes.
 # DEFAULT SETTINGS: "https://fr.openfoodfacts.org/cgi/search.pl".
-# CUSTOM SETTINGS: To change the country, use the appropriate  ISO-3166-1
+# CUSTOM SETTINGS: To change the country, use the appropriate ISO-3166-1
 # Alpha 2 code and replace it in the endpoint (e.g.
 # "https://es.openfoodfacts.org/cgi/search.pl").
 # For more information, please check "https://documenter.getpostman.com/view/
@@ -26,39 +26,39 @@ PRODUCTS_ENDPOINT = "https://fr.openfoodfacts.org/cgi/search.pl"
 
 # DESCRIPTION: Headers, i.e. the application general informations, among other,
 # its name and version number.
-# MANDATORY : Yes.
+# MANDATORY: Yes.
 # DEFAULT SETTINGS: {'User-Agent': 'HealthyProductApp - Web - Version 0.0'}.
 # CUSTOM SETTINGS: Settings can be changed but must keep the following
-# structure{'User-Agent': 'your informations'}.
-# For more informations, please check "https://documenter.getpostman.
+# structure {'User-Agent': 'your information'}.
+# For more information, please check "https://documenter.getpostman.
 # com/view/8470508/SVtN3Wzy?version=latest#intro".
 HEADER = {'User-Agent': 'HealthyProductApp - Web - Version 0.0'}
 
 # DESCRIPTION: The database name to use when connecting with the MySQL server.
-# MANDATORY : Yes.
+# MANDATORY: Yes.
 # DEFAULT SETTINGS: "HPA" (HealthyProduuctApp).
 # COMMENTS: Can be changed. Note that if a database using that name already
 # exists, it will be overwritten.
 DATABASE_NAME = "HPA"
 
 # DESCRIPTION: Host name i.e. The server on which MySQL is running.
-# MANDATORY : Yes.
+# MANDATORY: Yes.
 # DEFAULT SETTINGS: "localhost".
-# CUSTOM SETTINGS: Can be changed.Can be an IP adress as well if the
-#  MySQL serveris not the local machine.
-# For more informations, please check "https://dev.mysql.
+# CUSTOM SETTINGS: Can be changed. Can be an IP address as well if the
+# MySQL server is not the local machine.
+# For more information, please check "https://dev.mysql.
 # com/doc/connector-python/en/".
 HOST = "localhost"
 
 # DESCRIPTION: The user name used to authenticate with the MySQL server.
-# MANDATORY : Yes.
+# MANDATORY: Yes.
 # DEFAULT SETTINGS: "root".
 # CUSTOM SETTINGS: User must have at least the following MySQL
 # privileges CREATE, DROP, EXECUTE, INSERT, REFERENCES, SELECT,
 # and SHOW DATABASE.
-# For more informations, please check "https://dev.mysql.
+# For more information, please check "https://dev.mysql.
 # com/doc/connector-python/en/".
-# For more informations specifically on privileges, please check
+# For more information specifically on privileges, please check
 # "https://dev.mysql.com/doc/refman/5.7/en/grant.html#grant-privileges".
 USER = "root"
 
@@ -66,7 +66,7 @@ USER = "root"
 # MANDATORY: Yes.
 # DEFAULT SETTINGS: root.
 # CUSTOM SETTINGS: Must be the password of the user corresponding account.
-# For more informations, please check "https://dev.mysql.
+# For more information, please check "https://dev.mysql.
 # com/doc/connector-python/en/".
 PASSWORD = "root"
 
@@ -83,21 +83,21 @@ SELECTED_CATEGORIES = ["en:snacks", "en:desserts", "en:breads",\
 "en:breakfast-cereals", "en:meals"]
 
 # DESCRIPTION: Amount of product to get from OFF API per product category.
-# MANDATORY : Yes.
+# MANDATORY: Yes.
 # DEFAULT SETTINGS: 1000.
 # CUSTOM SETTINGS: Can be changed but should not exceed 2000 to avoid
 # upload failure.
 PRODUCTS_AMOUNT = 10
 
 # DESCRIPTION: Message displayed when user input number is out of range.
-# MANDATORY : Yes.
+# MANDATORY: Yes.
 # DEFAULT SETTINGS: "Only numbers included in above list can be used. Retry"
 # CUSTOM SETTINGS: Message can be changed but should keep sense.
 MESSAGE_OOR = "Only numbers included in the list can be used. Retry"
 
 # DESCRIPTION: Message displayed when user input should
 # be "y"(yes) or "n" (no) and is not.
-# MANDATORY : Yes.
+# MANDATORY: Yes.
 # DEFAULT SETTINGS: "Only numbers included in above list can be used. Retry"
 # CUSTOM SETTINGS: Message can be changed but should keep sense.
 MESSAGE_YN = "Answer must be y/n . Retry"
