a48c614bf02203f89375a7cf488ea15318e29da8
Thomas Piergiovanni <thomas.piergiovanni@gmail.com>
2020-08-04 13:40:47 +0200

    updated .giignore

--- program/admin/config.py
+++ program/admin/config.py
@@ -36,42 +36,6 @@ PRODUCTS_ENDPOINT = "https://fr.openfoodfacts.org/cgi/search.pl"
 # com/view/8470508/SVtN3Wzy?version=latest#intro".
 HEADER = {'User-Agent': 'HealthyProductApp - Web - Version 0.0'}
 
-# DESCRIPTION: The database name to use when connecting with the MySQL server.
-# MANDATORY: Yes.
-# DEFAULT SETTINGS: "HPA" (HealthyProduuctApp).
-# CUSTOM SETTINGS: Database name can be changed. Note that if a database using that name already
-# exists, it will be overwritten.
-DATABASE_NAME = "HPA"
-
-# DESCRIPTION: Host name i.e. The server on which MySQL is running.
-# MANDATORY: Yes.
-# DEFAULT SETTINGS: "localhost".
-# CUSTOM SETTINGS: Can be changed if different. Can be an IP address as well if the
-# MySQL server is not the local machine.
-# For more information, please check "https://dev.mysql.
-# com/doc/connector-python/en/".
-HOST = "localhost"
-
-# DESCRIPTION: The user name used to authenticate with the MySQL server.
-# MANDATORY: Yes.
-# DEFAULT SETTINGS: "root".
-# CUSTOM SETTINGS: User must have at least the following MySQL
-# privileges CREATE, DROP, EXECUTE, INSERT, REFERENCES, SELECT,
-# and SHOW DATABASE.
-# For more information, please check "https://dev.mysql.
-# com/doc/connector-python/en/".
-# For more information specifically on privileges, please check
-# "https://dev.mysql.com/doc/refman/5.7/en/grant.html#grant-privileges".
-USER = "root"
-
-# DESCRIPTION: The password to authenticate the user with the MySQL server.
-# MANDATORY: Yes.
-# DEFAULT SETTINGS: root.
-# CUSTOM SETTINGS: Must be the password of the user corresponding account.
-# For more information, please check "https://dev.mysql.
-# com/doc/connector-python/en/".
-PASSWORD = "root"
-
 # DESCRIPTION: OFF API products categories type used in the application.
 # MANDATORY: Yes.
 # DEFAULT SETTINGS: ["en:snacks", "en:desserts", "en:breads",
